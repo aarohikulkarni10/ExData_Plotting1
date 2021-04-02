e1671b9439aa4850aa3540443d19e25d76fffc18
aarohikulkarni10 <43353439+aarohikulkarni10@users.noreply.github.com>
2021-04-02 21:06:04 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,14 +1,23 @@
+  
+#PLOT3 R CODE 
+
+#Input data from txt file 
 datatxt <- "./household_power_consumption.txt"
 data <- read.table(datatxt, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#Extraction of only the required subset
+RequiredData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+GlobalActivePower <- as.numeric(RequiredData$Global_active_power)
+Sub_Metering1 <- as.numeric(RequiredData$Sub_metering_1)
+Sub_Metering2 <- as.numeric(RequiredData$Sub_metering_2)
+Sub_Metering3 <- as.numeric(RequiredData$Sub_metering_3)
+#png and pixels
 png("plot3.png", width=480, height=480)
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
+#defining the kind of graph to be plotted
+plot(datetime, Sub_Metering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, Sub_Metering2, type="l", col="red")
+lines(datetime, Sub_Metering3, type="l", col="blue")
+#defining legend 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+#Shutting down the specified device
 dev.off()
