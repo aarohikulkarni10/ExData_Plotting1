5f2b4f603d6586f5b78e4627cc38b2acd987a152
aarohikulkarni10 <43353439+aarohikulkarni10@users.noreply.github.com>
2021-04-02 20:59:12 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,15 @@
- datatxt <- "./household_power_consumption.txt"
+#PLOT2 R CODE 
+
+#Input data from txt file 
+datatxt <- "./household_power_consumption.txt"
 data <- read.table(datatxt, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+#Extraction of only the required subset
+RequiredData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(RequiredData$Date, RequiredData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(RequiredData$Global_active_power)
+#png and pixels defined 
 png("plot2.png", width=480, height=480)
+#plot
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+#Shutting down the specified device
 dev.off()
